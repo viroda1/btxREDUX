8dbdcb8b3a3c24a0b6dc089a83b9ef581f0d9b56
lithium_a2d <viroisda1@gmail.com>
2026-09-08 20:13:56 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+// main.js - orchestrator (already integrated in index.html)
+// This file is kept for structural completeness.
+export function init() {
+    console.log('REDUX{btx} initialized.');
+}
